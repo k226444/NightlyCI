bce3932431a7ed7355a10984a906fa685ffa01ad
k226444 <127910449+k226444@users.noreply.github.com>
2023-09-06 12:10:18 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ sudo xcodebuild -license
 
 #export PATH="'"$(python3 -m site --user-base)"'/bin:$PATH"
 #python3 -m pip install --user mercurial==6.1.4
-./hg pull https://hg.mozilla.org/mozilla-unified
+chmod +x /Users/runner/hg
+/Users/runner/hg pull https://hg.mozilla.org/mozilla-unified
 
 #python3 bootstrap.py --no-interactive --application-choice=browser
 
